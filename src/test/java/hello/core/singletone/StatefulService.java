097c45fd07c03ce332ff30956553d91ddf9b1c83
idiotengineer <gurtjd97@gmail.com>
2022-10-23 19:24:18 +0900

    Lesson 33 - 싱글톤 방식의 주의점

---
+++ src/test/java/hello/core/singletone/StatefulService.java
@@ -0,0 +1,18 @@
+package hello.core.singletone;
+
+public class StatefulService {
+    //private int price;
+/*
+    public void order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+    }*/
+    public int order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        return price;
+
+   /* public int getPrice() {
+        return price;
+    }*/
+    }
+}
