f81fee72f83c9244b8817dc409778d21cf8bb065
idiotengineer <gurtjd97@gmail.com>
2022-10-23 17:59:18 +0900

    Lesson 32 - 싱글톤 컨테이너

--- src/test/java/hello/core/singletone/SingletoneTest.java
+++ src/test/java/hello/core/singletone/SingletoneTest.java
@@ -5,6 +5,8 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -39,6 +41,23 @@ public class SingletoneTest {
 
 
         assertThat(singletoneService1).isSameAs(singletoneService2);
+    }
+
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        //참조값이 다름을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
+        // MemberService에는 어떠한 싱글톤 패턴 코드가 없지만, 자동 적용됨.
     }
 }
 
